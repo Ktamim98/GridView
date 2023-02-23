c83a39b65e107cd24f6c6e528c44b7b11b8624a8
Ktamim98 <tamim15-2515@diu.edu.bd>
2023-02-24 02:22:47 +0600

    Initial Commit

---
+++ project10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  project10
+//
+//  Created by Tamim Khan on 23/2/23.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var photos: String
+    
+    init(name: String, photos: String) {
+        self.name = name
+        self.photos = photos
+    }
+}
---
+++ project10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by Tamim Khan on 23/2/23.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
+
